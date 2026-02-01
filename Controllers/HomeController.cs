c6ed3c25175a4beae5fff8681bdc9a132e9af4c6
Murugesan U <murugesan1234u@gmail.com>
2026-02-02 02:25:29 +0530

    test1

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using WebApp1.Models;
 
 namespace WebApp1.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : Controlle
     {
         private readonly ILogger<HomeController> _logger;
 
